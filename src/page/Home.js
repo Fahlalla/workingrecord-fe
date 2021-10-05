7c2d6b339f8e730dc602903fd13f08604fcfc0a1
Tadsika <tadsika@odds.team>
2021-10-05 08:21:37 +0700

    modified Home.js

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,12 +1,17 @@
 const Home = () => {
   return (
     <div className="flex ...">
-      <div className="w-1/4 mx- bg-green-100">
+      <div className="w-1/4 text-center bg-green-100">
         <div className="my-8"> Individual</div>
         <div className="my-8">History</div>
         <div className="my-8" >Profile</div>
       </div>
-      <div className="w-3/4 bg-green-50">w-3/4</div>
+      <div className="w-3/4 bg-green-50">Homepage
+      <div className="my-2">The Kai Sarai</div>
+      <div className="my-2">Site - Mor Chana</div>
+      <div className="box-content h-10 w-10 p-4 border-4">เบิกเงิน</div>
+      </div>
+
     </div>
   );
 };
