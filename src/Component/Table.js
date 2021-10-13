0ed7937e9aef1777522f55e8b35ae8c6fe52c08e
FamieFame <fameanunn@odds.team>
2021-10-13 08:15:27 +0700

    Add component Table

---
+++ src/Component/Table.js
@@ -0,0 +1,84 @@
+import styled from "styled-components"
+
+const  Th = styled.div.attrs({
+    className: "w-1/4 border-2 border-purple-400",
+    
+})``;
+const Table = () => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <Th>Name</Th>
+                    <th className="w-1/4 border-2 border-blue-500">Email</th>
+                    <th className="w-1/4 border-2 border-green-300">Phone Number</th>
+                    <th className="w-1/4 border-2 border-yellow-300">Contact</th>
+                    <th className="border-2 border-red-300 w-1/8">Site</th>
+                    <th className="border-2 border-red-500 w-1/8">Status</th>
+                    <th className="border-2 border-pink-300 w-1/8">Bell</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td className="border-2 border-purple-400">
+                        Tadsika Khongkasawan
+                    </td>
+                    <td claasName="border-2 border-blue-500">Tadsika@odds.team</td>
+                    <td className="border-2 border-green-300">0818888888</td>
+                    <td className="border-2 border-yellow-300">Jill(0888888888)</td>
+                    <td className="border-2 border-red-300">odds</td>
+                    <td className="text-green-600 border-2 border-red-500">
+                        Submitted
+                    </td>
+                </tr>
+                <tr>
+                    <td className="border-2 border-purple-400">
+                        Tadsika Khongkasawan
+                    </td>
+                    <td claasName="border-2 border-blue-500">Tadsika@odds.team</td>
+                    <td className="border-2 border-green-300">0818888888</td>
+                    <td className="border-2 border-yellow-300">Jill(0888888888)</td>
+                    <td className="border-2 border-red-300">odds</td>
+                    <td className="text-green-600 border-2 border-red-500">
+                        Submitted
+                    </td>
+                </tr>
+                <tr>
+                    <td className="border-2 border-purple-400">
+                        Tadsika Khongkasawan
+                    </td>
+                    <td claasName="border-2 border-blue-500">Tadsika@odds.team</td>
+                    <td className="border-2 border-green-300">0818888888</td>
+                    <td className="border-2 border-yellow-300">Jill(0888888888)</td>
+                    <td className="border-2 border-red-300">odds</td>
+                    <td className="text-red-600 border-2 border-red-500 ">
+                        Not Submit
+                    </td>
+                </tr>
+                <tr>
+                    <td className="border-2 border-purple-400">
+                        Tadsika Khongkasawan
+                    </td>
+                    <td claasName="border-2 border-blue-500">Tadsika@odds.team</td>
+                    <td className="border-2 border-green-300">0818888888</td>
+                    <td className="border-2 border-yellow-300">Jill(0888888888)</td>
+                    <td className="border-2 border-red-300">odds</td>
+                    <td className="text-red-600 border-2 border-red-500 ">
+                        Not Submit
+                    </td>
+                </tr>
+                <tr>
+                    <td className="border-2 border-pink-900">pui pui</td>
+                    <td className="border-2 border-pink-900">pui@odds.Team</td>
+                    <td className="border-2 border-pink-900">0818888888</td>
+                    <td className="border-2 border-pink-900">Jill(0888888888)</td>
+                    <td className="border-2 border-pink-900">odds</td>
+                    <td className="text-red-600 border-2 border-pink-900 ">
+                        Not Submit
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+export default Table
--- src/page/Home.js
+++ src/page/Home.js
@@ -2,6 +2,7 @@ import Menubar from "../Component/Menubar"
 import Profile from "../Component/Profile"
 import Button from "../Component/button";
 import Search from "../Component/search";
+import Table from "../Component/Table";
 
 const Home = () => {
   return (
@@ -10,10 +11,11 @@ const Home = () => {
       <Profile />
       <Button />
       <Search />
+      <Table />
     </div>
 
     // <div className="flex">
-    //   <div className="w-1/6 text-white text-center bg-blue-300">
+    //   <div className="w-1/6 text-center text-white bg-blue-300">
     //     <div className="my-8"> Individual</div>
     //     <div className="my-8">History</div>
     //     <div className="my-8">Profile</div>
@@ -29,7 +31,7 @@ const Home = () => {
     //         </div>
     //       </div>
 
-    //       <div className="flex flex-wrap justify-end content-center">
+    //       <div className="flex flex-wrap content-center justify-end">
     //         <div className="flex flex-col">
     //           <div>
     //             <button className="w-56 h-12 text-white bg-black rounded-md">
@@ -52,7 +54,7 @@ const Home = () => {
     //             width="20"
     //             height="20"
     //             fill="currentColor"
-    //             className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+    //             className="absolute text-gray-400 transform -translate-y-1/2 left-3 top-1/2"
     //           >
     //             <path
     //               fill-rule="evenodd"
@@ -61,13 +63,13 @@ const Home = () => {
     //             />
     //           </svg>
     //           <input
-    //             className=" w-80 focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10"
+    //             className="py-2 pl-10 text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md w-80 focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none"
     //             type="text"
     //             aria-label="Search"
     //             placeholder="อยากค้นหาอะไรพิมพ์มาเลย"
     //           />{" "}
     //         </div>
-    //         <button className="flex items-center justify-center w-36 h-10 text-indigo-600 border border-indigo-300 rounded-md">
+    //         <button className="flex items-center justify-center h-10 text-indigo-600 border border-indigo-300 rounded-md w-36">
     //           Search
     //         </button>
     //       </div>
@@ -80,9 +82,9 @@ const Home = () => {
     //           <th className="w-1/4 border-2 border-blue-500">Email</th>
     //           <th className="w-1/4 border-2 border-green-300">Phone Number</th>
     //           <th className="w-1/4 border-2 border-yellow-300">Contact</th>
-    //           <th className="w-1/8 border-2 border-red-300">Site</th>
-    //           <th className="w-1/8 border-2 border-red-500">Status</th>
-    //           <th className="w-1/8 border-2 border-pink-300">Bell</th>
+    //           <th className="border-2 border-red-300 w-1/8">Site</th>
+    //           <th className="border-2 border-red-500 w-1/8">Status</th>
+    //           <th className="border-2 border-pink-300 w-1/8">Bell</th>
     //         </tr>
     //       </thead>
     //       <tbody>
@@ -94,7 +96,7 @@ const Home = () => {
     //           <td className="border-2 border-green-300">0818888888</td>
     //           <td className="border-2 border-yellow-300">Jill(0888888888)</td>
     //           <td className="border-2 border-red-300">odds</td>
-    //           <td className="border-2 border-red-500 text-green-600">
+    //           <td className="text-green-600 border-2 border-red-500">
     //             Submitted
     //           </td>
     //         </tr>
@@ -106,7 +108,7 @@ const Home = () => {
     //           <td className="border-2 border-green-300">0818888888</td>
     //           <td className="border-2 border-yellow-300">Jill(0888888888)</td>
     //           <td className="border-2 border-red-300">odds</td>
-    //           <td className="border-2 border-red-500 text-green-600">
+    //           <td className="text-green-600 border-2 border-red-500">
     //             Submitted
     //           </td>
     //         </tr>
