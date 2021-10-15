b6ca3cc5fa141e3a44467565de931966c993f70e
FamieFame <fameanunn@odds.team>
2021-10-15 08:18:04 +0700

    edit home style

--- src/Component/Profile.js
+++ src/Component/Profile.js
@@ -2,8 +2,8 @@ import image from "../Asset/Profile.png";
 
 const Profile = () => {
     return (
-        <div className="flex">
-            <img src={image} alt="Profile" width="200" height="200" />
+        <div className="flex h-auto">
+            <img className="flex items-center justify-center w-24 h-24 mr-2 rounded-full" src={image} alt="Profile" />
             <div>
               <div>ชื่อ ...</div>
               <div>รายได้สุทธิของคุณ 0.00 บาท</div>
