151b045d73cea2104e750c20772074736803ce9c
FamieFame <fameanunn@odds.team>
2021-10-15 08:36:42 +0700

    refactor remove unused

--- src/Component/button.js
+++ src/Component/button.js
@@ -2,11 +2,9 @@ const Button = () => {
     return(
         <div className="flex flex-wrap content-center justify-end">
             <div className="flex flex-col">
-              <div>
                 <button className="w-40 h-12 mb-2 text-white bg-black rounded-md">
                   Add income
                 </button>
-              </div>
               <div>
                 <button className="w-40 h-12 border border-gray-300 rounded-md">
                   Issue Problem
