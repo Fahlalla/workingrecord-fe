f6e26906938f334558a0f8b1237b304e57d25df4
Glock Rastarfah <glock-odds@Glocks-MacBook-Pro.local>
2021-10-19 00:37:05 +0700

    Glock Chang UI Working Racord

--- src/Component/Profile.js
+++ src/Component/Profile.js
@@ -1,16 +1,16 @@
 import image from "../Asset/Profile.png";
 
 const Profile = () => {
-    return (
-        <div className="flex h-auto">
-            <img className="flex items-center justify-center w-24 h-24 mr-2 rounded-full" src={image} alt="Profile" />
-            <div>
-              <div>ชื่อ ...</div>
-              <div>รายได้สุทธิของคุณ 0.00 บาท</div>
-              <div>บันทึก : อยากได้เงินก็กรอกมาสิ</div>
-            </div>
-          </div>
-    )
-} 
+  return (
+    <div className="flex h-auto">
+      <img className="flex items-center justify-center w-24 h-24 mr-2 rounded-full" src={image} alt="Profile" />
+      <div className="pl-12">
+        <div className="flex text-2xl pb-5 black">สำรวย หวยสำราญ</div>
+        <div className="pb-5">รายได้สุทธิของคุณ 0.00 บาท</div>
+        <div className="pb-5">บันทึก : อยากได้เงินก็กรอกมาสิ</div>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
--- src/Component/search.js
+++ src/Component/search.js
@@ -1,33 +1,33 @@
 const Search = () => {
-    return(
-                <form class="relative ml-6 list-disc">
-          <div className="flex justify-end">
-            <div className="relative">
-              <svg
-                width="20"
-                height="20"
-                fill="currentColor"
-                className="absolute text-gray-400 transform -translate-y-1/2 left-3 top-1/2"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                />
-              </svg>
-              <input
-                className="py-2 pl-10 text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md w-80 focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none"
-                type="text"
-                aria-label="Search"
-                placeholder="อยากค้นหาอะไรพิมพ์มาเลย"
-              />{" "}
-            </div>
-            <button className="flex items-center justify-center h-10 ml-2 text-gray-100 bg-indigo-600 rounded-md w-36">
-              Search
-            </button>
-          </div>
-        </form>
-    )
-}
+  return (
+    <form class="relative ml-6 list-disc">
+      <div className="flex justify-end">
+        <div className="relative">
+          <svg
+            width="20"
+            height="20"
+            fill="currentColor"
+            className="absolute text-gray-400 transform -translate-y-1/2 left-3 top-1/2"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+            />
+          </svg>
+          <input
+            className="py-2 pl-10 text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md w-80 focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none"
+            type="text"
+            aria-label="Search"
+            placeholder="อยากได้อะไรก็ค้นหาเอาเอง จุ๊บ จุ๊บ"
+          />{" "}
+        </div>
+        <button className="flex items-center justify-center h-9 ml-2 text-gray-100 bg-black rounded-md w-32">
+          ค้นหา
+        </button>
+      </div>
+    </form>
+  );
+};
 
-export default Search
+export default Search;
