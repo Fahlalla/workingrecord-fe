81da90b495fa87f38d561a1e3bb847b167c7de54
PunyaweeW <punyawee_w@odds.team>
2021-10-19 00:52:32 +0700

    Refactor to flex layout

--- src/Component/Menubar.js
+++ src/Component/Menubar.js
@@ -1,12 +1,35 @@
-const Menubar = () => {
-    return (
-        <div className="flex">
-          <div className="fixed h-screen px-20 text-center text-white bg-blue-300">
-            <div className="my-8"> Individual</div>
-            <div className="my-8">History</div>
-            <div className="my-8">Profile</div>
-          </div>
-        </div>
-)}
+import logo from "../Asset/OddsLogo.png";
+import historyIcon from "../Asset/History.png";
+import wallIcon from "../Asset/Wall.png";
+import profileIcon from "../Asset/ProfileMenuIcon.png";
+import issueProblem from "../Asset/IssueProblem.png";
 
-export default Menubar
+const MenuItem = (props) => {
+  const textColor = props.isActivate ? "#00B6EF" : "#ffffff";
+  return (
+    <div className="flex ml-2 mb-6">
+      <img
+        className="w-6 h-6 mr-4"
+        src={props.icon ? props.icon : logo}
+        alt="img-menu-item"
+      ></img>
+      <a href="/" style={{ color: textColor }}>
+        {props.text}
+      </a>
+    </div>
+  );
+};
+
+const Menubar = () => {
+  return (
+    <div className="h-screen p-4 text-white lg:w-64">
+      <img className="mb-10 ml-2 mt-6" src={logo} alt="odds-logo" />
+      <MenuItem isActivate={true} text="Individual" icon={wallIcon} />
+      <MenuItem text="History" icon={historyIcon} />
+      <MenuItem text="Profile" icon={profileIcon} />
+      <MenuItem text="Issue Problem" icon={issueProblem} />
+    </div>
+  );
+};
+
+export default Menubar;
--- src/page/Home.js
+++ src/page/Home.js
@@ -3,26 +3,22 @@ import Profile from "../Component/Profile";
 import Button from "../Component/button";
 import Search from "../Component/search";
 import Table from "../Component/Table";
+import "../index.css";
 
 const Home = () => {
   return (
-    <section className="grid grid-cols-6 gap-2">
-      <div className="col-span-1 col-start-1 w-max">
+    <div className="flex">
+      <aside className="h-screen sticky top-0 bg-side-color">
         <Menubar />
-      </div>
-      <div className="col-start-2 col-end-7">
-        <div className="flex justify-between h-auto p-6 m-4 border-2 border-gray-300 rounded-md">
-          <Profile />
-          <Button />
+      </aside>
+      <main className="flex-1 flex-col bg-purple-600">
+        <div className="mx-6">
+          <div className="bg-red-200 w-full h-screen"></div>
+          <div className="bg-blue-200 w-full h-screen"></div>
+          <div className="bg-green-200 w-full h-screen"></div>
         </div>
-        <div className="mb-4 mr-4">
-          <Search />
-        </div>
-        <div className="p-8 m-4 border-2 border-gray-300 rounded-md">
-          <Table />
-        </div>
-      </div>
-    </section>
+      </main>
+    </div>
   );
 };
 export default Home;
