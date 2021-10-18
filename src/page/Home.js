575d985726a3ee240864d8e4881bda2ac46c7fae
bambamot <71121623+vbamss@users.noreply.github.com>
2021-10-19 00:35:55 +0700

    update menubar and responsive

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,16 +1,16 @@
-import Menubar from "../Component/Menubar"
-import Profile from "../Component/Profile"
+import Menubar from "../Component/Menubar";
+import Profile from "../Component/Profile";
 import Button from "../Component/button";
 import Search from "../Component/search";
 import Table from "../Component/Table";
 
 const Home = () => {
   return (
-    <section className="grid grid-cols-6 gap-2">
+    <section className="flex h-screen overflow-hidden">
       <div className="col-span-1 col-start-1 w-max">
         <Menubar />
       </div>
-      <div className="col-start-2 col-end-7">
+      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
         <div className="flex justify-between h-auto p-6 m-4 border-2 border-gray-300 rounded-md">
           <Profile />
           <Button />
