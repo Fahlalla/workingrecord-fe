663bd6a867823340338d6bface75869a4ad7d8d4
pui🦦 <pui@odds.team>
2021-10-20 08:50:29 +0700

    add Profile Infomation component

---
+++ src/Component/Profile/SiteInfo.js
@@ -0,0 +1,13 @@
+const SiteInfo = () => {
+    return (
+        <div className="flex h-auto">
+            <div className="flex flex-col space-y-5">
+                <div className="font-bold text-4xl">ชื่อ ...</div>
+                <div className="text-xl">รายได้สุทธิของคุณ 0.00 บาท</div>
+                <div className="text-xl">บันทึก : อยากได้เงินก็กรอกมาสิ</div>
+            </div>
+        </div>
+    );
+};
+
+export default SiteInfo;
--- src/page/Profile.js
+++ src/page/Profile.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import Menubar from "../Component/Menubar";
+import PersonalInfo from "../Component/Profile/PersonalInfo";
+import SiteInfo from "../Component/Profile/SiteInfo";
+import BankAccountInfo from "../Component/Profile/BankAccountInfo";
 
-export default function Profile() {
+const ProfilePage = () => {
     return (
-        <div>
-            hello
+        <div className="flex h-full w-full">
+            <div className="flex items-start content-start h-full">
+                <Menubar />
+            </div>
+            <div className="flex flex-col w-full pt-4 px-12">
+                <PersonalInfo />
+                <SiteInfo />
+                <BankAccountInfo />
+            </div>
         </div>
-    )
-}
+
+    );
+};
+export default ProfilePage;
+
+
