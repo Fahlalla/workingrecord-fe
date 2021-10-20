f1f96e1a44f7686c1fb27583ac76b1d7b6feb66a
bambamot <71121623+vbamss@users.noreply.github.com>
2021-10-20 09:03:55 +0700

    add profile input component

--- src/Component/Profile/PersonalInfo.js
+++ src/Component/Profile/PersonalInfo.js
@@ -1,11 +1,9 @@
+import ProfileInput from "../Profile/ProfileInput.js"
+
 const PersonalInfo = () => {
     return (
         <div className="flex h-auto">
-            <div className="flex flex-col space-y-5">
-                <div className="font-bold text-4xl">ชื่อ ...</div>
-                <div className="text-xl">รายได้สุทธิของคุณ 0.00 บาท</div>
-                <div className="text-xl">บันทึก : อยากได้เงินก็กรอกมาสิ</div>
-            </div>
+            <ProfileInput/>
         </div>
     );
 };
---
+++ src/Component/Profile/ProfileInput.js
@@ -0,0 +1,13 @@
+const SiteInfo = () => {
+  return (
+    <div className="flex h-auto">
+      <div>ชื่อ</div>
+      <input
+        className="py-2 pl-10 text-sm text-black shadow-md placeholder-gray-500 border border-gray-200 rounded-md w-80 focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none"
+        type="text"
+      />
+    </div>
+  );
+};
+
+export default SiteInfo;
