e41d1ab9f610c18820d05c073872765fe41c720b
pornpavit sanusit <pornpavitsanusit@ip-172-16-4-199.ap-southeast-1.compute.internal>
2021-10-21 09:01:47 +0700

    Add table and fix ui in summary page

--- src/Component/Summary.js
+++ src/Component/Summary.js
@@ -1,5 +1,8 @@
 import Menubar from "./Menubar";
 import styled from "styled-components";
+import Table from "./Table";
+import Button from "./button";
+import Search from "./search";
 
 const ContainerTabMenu = styled.div.attrs({})`
   border-bottom: 2px solid #eaeaea;
@@ -10,21 +13,35 @@ const Summary = () => {
     <div className="flex h-full w-full">
       <div className="flex items-start content-start h-full">
         <Menubar />
-        <div className="flex flex-col w-full pt-4 px-12">
-          <div>Summary</div>
-          {/* 
-          <div style="border-bottom: 2px solid #eaeaea"> */}
+      </div>
+      <div className="w-full pt-4 px-12">
+        <div className="text-3xl mb-14 font-bold">Summary</div>
+        <div className="flex justify-between">
           <ContainerTabMenu>
             <ul className="flex cursor-pointer">
-              <li className="py-2 px-6 bg-white rounded-t-lg">Personal</li>
-              <li className="py-2 px-6 bg-white rounded-t-lg text-gray-500 bg-gray-200">
-                Akun
+              <li className="py-2 px-6 bg-white rounded-t-lg">
+                รายการที่กรอกแล้ว
               </li>
               <li className="py-2 px-6 bg-white rounded-t-lg text-gray-500 bg-gray-200">
-                Pengaturan
+                คนที่กรอกแล้ว
+              </li>
+              <li className="py-2 px-6 bg-white rounded-t-lg text-gray-500 bg-gray-200">
+                คนที่ยังไม่ได้กรอก
               </li>
             </ul>
           </ContainerTabMenu>
+          <div className="">
+            <Button />
+          </div>
+        </div>
+
+        <div className="flex flex-col w-full pt-4 px-12">
+          <div className="mt-8">
+            <Search />
+          </div>
+          <div className="mt-8">
+            <Table />
+          </div>
         </div>
       </div>
     </div>
