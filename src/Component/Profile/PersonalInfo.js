09a0fcbf9965925e1c8ee71d5ee1bb78bd480e43
bambamot <71121623+vbamss@users.noreply.github.com>
2021-10-21 09:03:32 +0700

    update profile input

--- src/Component/Profile/PersonalInfo.js
+++ src/Component/Profile/PersonalInfo.js
@@ -1,23 +1,31 @@
 import ProfileInput from "../Profile/ProfileInput.js";
 
 const PersonalInfo = () => {
-    return (
-        <div className="flex h-auto">
-            <div className="flex-col space-y-5">
-                <div className="flex space-x-10">
-                    <ProfileInput />
-                    <ProfileInput />
-                    <ProfileInput />
-                </div>
-                <div className="flex space-x-10">
-                    <ProfileInput />
-                    <ProfileInput />
-                    <ProfileInput />
-                </div>
-            </div>
-
+  return (
+    <div className="flex h-auto">
+      <div className="flex-col space-y-5">
+        <div className="flex space-x-10">
+          <ProfileInput label="ชื่อ" />
+          <ProfileInput label="นามสกุล" />
+          <ProfileInput label="เลขบัตรประชาชน" />
         </div>
-    );
+        <div className="flex space-x-10">
+          <ProfileInput label="วัน เดือน ปีเกิด" />
+          <ProfileInput label="เบอร์โทรศัพท์" />
+          <ProfileInput label="E-mail" disabled />
+        </div>
+        <div className="flex space-x-10">
+          <ProfileInput label="ที่อยู่" />
+          <ProfileInput label="จังหวัด" />
+          <ProfileInput label="เขต/อำเภอ" />
+        </div>
+        <div className="flex space-x-10">
+          <ProfileInput label="แขวง/ตำบล" />
+          <ProfileInput label="รหัสไปรษณีย์" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PersonalInfo;
--- src/Component/Profile/ProfileInput.js
+++ src/Component/Profile/ProfileInput.js
@@ -1,12 +1,14 @@
-const ProfileInput = () => {
+const ProfileInput = ({ label, disabled }) => {
   return (
     <div className="flex-col">
-      <div>ชื่อ</div>
+      <div>{label}</div>
       <input
-        className="py-2 pl-10 text-sm text-black shadow-md placeholder-gray-500 border border-gray-200 rounded-md w-80 focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none"
+        disabled={disabled}
+        className={`py-2 pl-10 text-sm text-black shadow-md border border-gray-200 rounded-md w-80 focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none ${
+          disabled && "bg-gray-300"
+        }`}
         type="text"
       />
-
     </div>
   );
 };
