80e8169c3063269d8d246834e8e17c53d05a6929
pui🦦 <pui@odds.team>
2021-10-21 08:44:07 +0700

    edit PersonalInfo

--- src/Component/Profile/PersonalInfo.js
+++ src/Component/Profile/PersonalInfo.js
@@ -1,9 +1,21 @@
-import ProfileInput from "../Profile/ProfileInput.js"
+import ProfileInput from "../Profile/ProfileInput.js";
 
 const PersonalInfo = () => {
     return (
         <div className="flex h-auto">
-            <ProfileInput/>
+            <div className="flex-col space-y-5">
+                <div className="flex space-x-10">
+                    <ProfileInput />
+                    <ProfileInput />
+                    <ProfileInput />
+                </div>
+                <div className="flex space-x-10">
+                    <ProfileInput />
+                    <ProfileInput />
+                    <ProfileInput />
+                </div>
+            </div>
+
         </div>
     );
 };
