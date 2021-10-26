bb837ef7734fe0a9b964f918f9e4d56c06f59e31
TheKai <the_kai_702@hotmail.com>
2021-10-26 08:26:10 +0700

    add DatePicker

--- src/Component/DatePicker.js
+++ src/Component/DatePicker.js
@@ -1,4 +1,21 @@
-const DatePicker = () => {
-  return <div>Continue...</div>;
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+
+// import required css from library
+import "react-datepicker/dist/react-datepicker.css";
+
+// main implementation. using selected and onChange as main props to get and change the selected date value
+const DatePickers = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  return (
+    // <div className="border-gray-400">
+    <DatePicker
+      className="bg-green-500"
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+    />
+    // </div>
+  );
 };
-export default DatePicker;
+
+export default DatePickers;
--- src/Component/Summary.js
+++ src/Component/Summary.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Table from "./Table";
 import Button from "./button";
 import Search from "./search";
+import DatePickers from "./DatePicker";
 
 const ContainerTabMenu = styled.div.attrs({})`
   border-bottom: 2px solid #eaeaea;
@@ -17,21 +18,28 @@ const Summary = () => {
       <div className="w-full pt-4 px-12">
         <div className="text-3xl mb-14 font-bold">Summary</div>
         <div className="flex justify-between">
-          <ContainerTabMenu>
-            <ul className="flex cursor-pointer">
-              <li className="py-2 px-6 bg-white rounded-t-lg">
-                รายการที่กรอกแล้ว
-              </li>
-              <li className="py-2 px-6 bg-white rounded-t-lg text-gray-500 bg-gray-200">
-                คนที่กรอกแล้ว
-              </li>
-              <li className="py-2 px-6 bg-white rounded-t-lg text-gray-500 bg-gray-200">
-                คนที่ยังไม่ได้กรอก
-              </li>
-            </ul>
-          </ContainerTabMenu>
-          <div className="">
-            <Button />
+          <div>
+            <ContainerTabMenu>
+              <ul className="flex cursor-pointer">
+                <li className="py-2 px-6 bg-white rounded-t-lg">
+                  รายการที่กรอกแล้ว
+                </li>
+                <li className="py-2 px-6 bg-white rounded-t-lg text-gray-500 bg-gray-200">
+                  คนที่กรอกแล้ว
+                </li>
+                <li className="py-2 px-6 bg-white rounded-t-lg text-gray-500 bg-gray-200">
+                  คนที่ยังไม่ได้กรอก
+                </li>
+              </ul>
+            </ContainerTabMenu>
+          </div>
+          <div className="flex justify-end">
+            <div className="rounded-md">
+              <DatePickers />
+            </div>
+            <div>
+              <Button />
+            </div>
           </div>
         </div>
 
