07065e711c457586405f35074df5b4d37769f4fd
pui🦦 <pui@odds.team>
2021-10-26 08:25:34 +0700

    Update dropdown

--- src/Component/Profile/DropdownInput.js
+++ src/Component/Profile/DropdownInput.js
@@ -1,13 +1,17 @@
-const DropdownInput = ({ label, option }) => {
+const DropdownInput = ({ label, options }) => {
+  let optionList =
+    options.length > 0 &&
+    options.map((item, i) => {
+      return <option>{item.value}</option>;
+    });
+
   return (
     <div className="flex-col">
       <div>{label}</div>
       <select
         className={`py-2 pl-10 text-sm text-black shadow-md border border-gray-200 rounded-md w-80 focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none`}
       >
-        <option value="">1</option>
-        <option value="">1</option>
-        <option value="">1</option>
+        {optionList}
       </select>
     </div>
   );
--- src/Component/Profile/PersonalInfo.js
+++ src/Component/Profile/PersonalInfo.js
@@ -3,6 +3,11 @@ import UploadFile from "./UploadFile.js";
 import DropdownInput from "./DropdownInput.js";
 
 const PersonalInfo = () => {
+  const province = [
+    { value: "Bangkok" },
+    { value: "Khon Kaen" },
+    { value: "Nan" },
+  ];
   return (
     <div className="flex h-auto">
       <div className="flex-col space-y-5">
@@ -18,11 +23,11 @@ const PersonalInfo = () => {
         </div>
         <div className="flex space-x-10">
           <ProfileInput label="ที่อยู่" />
-          <DropdownInput label="จังหวัด" />
-          <DropdownInput label="เขต/อำเภอ" />
+          <DropdownInput label="จังหวัด" options={province} />
+          <DropdownInput label="เขต/อำเภอ" options={[]} />
         </div>
         <div className="flex space-x-10">
-          <DropdownInput label="แขวง/ตำบล" />
+          <DropdownInput label="แขวง/ตำบล" options={[]} />
           <ProfileInput label="รหัสไปรษณีย์" />
         </div>
         <div className="flex space-x-10">
