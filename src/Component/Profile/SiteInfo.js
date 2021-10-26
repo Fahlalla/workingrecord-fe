af38ab8c5577ab647d2bc08fdf7c5ee0accf6687
bambamot <71121623+vbamss@users.noreply.github.com>
2021-10-26 09:01:29 +0700

    Update Site

--- src/Component/Profile/SiteInfo.js
+++ src/Component/Profile/SiteInfo.js
@@ -1,11 +1,25 @@
-const SiteInfo = () => {
-    return (
-        <div className="flex h-auto">
-            <div className="flex flex-col space-y-5">
+import ProfileInput from "../Profile/ProfileInput.js";
 
-            </div>
+const SiteInfo = () => {
+  return (
+    <div className="flex flex-row h-auto">
+      <div className="">
+        <h1>Site</h1>
+      </div>
+      <div className="flex-col space-y-5">
+        <div className="flex space-x-10">
+          <ProfileInput label="Site" />
+          <ProfileInput label="Team" />
+          <ProfileInput label="ผู้แนะนำเข้า Odds" />
         </div>
-    );
+        <div className="flex space-x-10">
+          <ProfileInput label="Discord Account" />
+          <ProfileInput label="รายได้ต่อวัน" />
+          <ProfileInput label="รายได้ต่อชั่วโมง (Part-time)" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SiteInfo;
