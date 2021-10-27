e200981524a7808802ef420205c446986a1360a6
pui <pui@odds.team>
2021-10-27 08:54:30 +0700

    Add BankAccountInfo

--- src/Component/Profile/BankAccountInfo.js
+++ src/Component/Profile/BankAccountInfo.js
@@ -1,13 +1,33 @@
+import DropdownInput from "./DropdownInput";
+import ProfileInput from "./ProfileInput";
+import UploadFile from "./UploadFile";
+
 const BankAccountInfo = () => {
-    return (
-        <div className="flex h-auto">
-            <div className="flex flex-col space-y-5">
-                <div className="font-bold text-4xl">ชื่อ ...</div>
-                <div className="text-xl">รายได้สุทธิของคุณ 0.00 บาท</div>
-                <div className="text-xl">บันทึก : อยากได้เงินก็กรอกมาสิ</div>
-            </div>
+  const province = [
+    { value: "Krungthai (KTB)" },
+    { value: "Kasikorn (KBANK)" },
+    { value: "Krungsri (BAY)" },
+    { value: "Siam Commercial Bank (SCB)" },
+    { value: "Bangkok Bank (BBL)" },
+  ];
+  return (
+    <div className="flex h-auto">
+      <div className="flex flex-col space-y-5 pt-5">
+        <div className="font-bold text-2xl">บัญชีธนาคาร</div>
+        <div className="flex space-x-10">
+          <ProfileInput label="ชื่อบัญชี" />
+          <DropdownInput label="ธนาคาร" options={province} />
+          <ProfileInput label="สาขา" />
         </div>
-    );
+        <div className="flex space-x-10">
+          <ProfileInput label="เลขที่บัญชี" />
+        </div>
+        <div className="flex space-x-10">
+          <UploadFile label="รูปบัญชีธนาคาร" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BankAccountInfo;
