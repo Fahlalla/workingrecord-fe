5860cd0b88f9794e0e3e1f7314914c2c7968fbb4
Jaarukit <jarukit.p@kkumail.com>
2021-10-28 08:46:30 +0700

    Add icon date picker.

--- src/Component/DatePicker.js
+++ src/Component/DatePicker.js
@@ -3,20 +3,26 @@ import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
 import th from "date-fns/locale/th";
+import DatePickerIcon from "../Asset/DatePicker.svg";
 
 const DatePickers = () => {
   const [startDate, setStartDate] = useState(new Date());
 
   return (
-    // <div className="border-gray-400">
-    <DatePicker
-      className="py-3 px-3"
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      dateFormat="MMMM yyyy"
-      locale={th}
-    />
-    // </div>
+    <label className="flex">
+      <DatePicker
+        className="py-3 w-32 text-center"
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        dateFormat="MMMM yyyy"
+        locale={th}
+      />
+      <img
+        src={DatePickerIcon}
+        alt="Datepicker Icon"
+        className="cursor-pointer"
+      />
+    </label>
   );
 };
 
