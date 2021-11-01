8a6beacb40b44d47e80b6a382b1783bc95ad9ded
mindpakawat <48022706+mindpakawat@users.noreply.github.com>
2021-11-01 09:09:26 +0700

    Change month to local string

--- src/Component/WorklogSum.js
+++ src/Component/WorklogSum.js
@@ -2,7 +2,10 @@ const WorklogSum = (props) => {
   return (
     <div className="flex flex-col space-y-5 ">
       <div className="font-bold text-4xl">สรุปรายการขอเบิก Worklog</div>
-      <div className="text-xl">ประจำเดือน {(props.month.getMonth() + 1).toString()} ณ วันที่ 25</div>
+      <div className="text-xl">
+        ประจำเดือน {props.month.toLocaleString("default", { month: "long" })} ณ
+        วันที่ 25
+      </div>
     </div>
   );
 };
