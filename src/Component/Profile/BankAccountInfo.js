7b45229940f39578a68433e5d05d9ef9446df5a6
Chalittaya <chalittaya@kkumail.com>
2021-11-04 08:33:39 +0700

    bankAccountInfo add value

--- src/Component/Profile/BankAccountInfo.js
+++ src/Component/Profile/BankAccountInfo.js
@@ -1,6 +1,7 @@
 import DropdownInput from "./DropdownInput";
 import ProfileInput from "./ProfileInput";
 import UploadFile from "./UploadFile";
+import { useState, useEffect } from "react";
 
 const BankAccountInfo = () => {
   const province = [
@@ -10,17 +11,39 @@ const BankAccountInfo = () => {
     { value: "Siam Commercial Bank (SCB)" },
     { value: "Bangkok Bank (BBL)" },
   ];
+
+  const [bankAccount,setBankAccount] = useState("")
+  const [bankBranch,setBankBranch] = useState("")
+  const [bankAccountNumber,setBankAccountNumber] = useState("")
+
+  let handleChangebankAccount=(e) => {
+    setBankAccount(e.target.value);
+  }
+  let handleChangebankBrancht=(e) => {
+    setBankBranch(e.target.value);
+  }
+  let handleChangebankAccountNumber=(e) => {
+    setBankAccountNumber(e.target.value);
+  }
+
+  useEffect(() => {
+    console.log(bankAccount)
+    console.log(bankBranch)
+    console.log(bankAccountNumber)
+
+  }, [bankAccount, bankBranch, bankAccountNumber])
+
   return (
     <div className="flex h-auto">
       <div className="flex flex-col space-y-5 pt-5">
         <div className="font-bold text-2xl">บัญชีธนาคาร</div>
         <div className="flex space-x-10">
-          <ProfileInput label="ชื่อบัญชี" />
+          <ProfileInput label="ชื่อบัญชี" onChange={handleChangebankAccount} value={bankAccount}/>
           <DropdownInput label="ธนาคาร" options={province} />
-          <ProfileInput label="สาขา" />
+          <ProfileInput label="สาขา" onChange={handleChangebankBrancht} value={bankBranch}/>
         </div>
         <div className="flex space-x-10">
-          <ProfileInput label="เลขที่บัญชี" />
+          <ProfileInput label="เลขที่บัญชี" onChange={handleChangebankAccountNumber} value={bankAccountNumber}/>
         </div>
         <div className="flex space-x-10">
           <UploadFile label="รูปบัญชีธนาคาร" />
