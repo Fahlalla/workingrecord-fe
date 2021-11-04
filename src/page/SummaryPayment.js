f2dead7a9ded066d8099df5f02e7bc1cbd732b5a
mindpakawat <48022706+mindpakawat@users.noreply.github.com>
2021-11-04 09:08:23 +0700

    Add popup component

--- src/page/SummaryPayment.js
+++ src/page/SummaryPayment.js
@@ -4,36 +4,42 @@ import WorklogSum from "../Component/WorklogSum.js";
 import SummaryTable from "../Component/SummaryTable.js";
 import MonthPicker from "../Component/MonthPicker.js";
 import { AdminProfiles } from "../AdminProfiles.js";
-
+import PopupConfirm from "../Component/PopupConfirm.js";
 
 const SummaryPayment = () => {
   const [selectMonth, setSelectMonth] = useState(new Date());
+  const showPopup = () => {};
   return (
     <div className="flex flex-col w-full px-12 pt-4 ">
       <div className="flex justify-between space-y-2">
         <WorklogSum month={selectMonth} />
         <div>
-          <MonthPicker month={selectMonth} setSelectMonth={setSelectMonth}/>
+          <MonthPicker month={selectMonth} setSelectMonth={setSelectMonth} />
         </div>
       </div>
       <div className="flex justify-between mt-8">
         {AdminProfiles.map((adminProfile) => {
-          return (
-            <Card data={adminProfile} key={adminProfile.id}/>
-          )
+          return <Card data={adminProfile} key={adminProfile.id} />;
         })}
       </div>
       <div className="mt-8">
         <SummaryTable />
-        <div className="flex justify-center text-lg font-bold">รวมยอดเงิน 3,000,000 บาท </div>
+        <div className="flex justify-center text-lg font-bold">
+          รวมยอดเงิน 3,000,000 บาท{" "}
+        </div>
         <div></div>
         <div>
-         <div className="flex flex-wrap content-center justify-center">
-             <button className="text-lg font-bold text-white rounded-full w-80 h-14 " style = {{backgroundColor:'#00B6EF'}}>
-                ยืนยันการโอนครบถ้วน และส่งให้บัญชี
-              </button>
+          <div className="flex flex-wrap content-center justify-center">
+            <button
+              className="text-lg font-bold text-white rounded-full w-80 h-14 "
+              style={{ backgroundColor: "#00B6EF" }}
+              onclick={() => showPopup()}
+            >
+              ยืนยันการโอนครบถ้วน และส่งให้บัญชี
+            </button>
           </div>
-      </div>
+          <PopupConfirm />
+        </div>
       </div>
     </div>
   );
