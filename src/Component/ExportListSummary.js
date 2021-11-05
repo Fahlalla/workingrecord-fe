915188a848932bd10c8c15ec6174812d8e0cedfc
ศุภณัฐ ชินราช <45527907+dudkinox@users.noreply.github.com>
2021-11-05 08:48:43 +0700

    mock Exported

--- src/Component/ExportListSummary.js
+++ src/Component/ExportListSummary.js
@@ -1,119 +1,128 @@
-const Table = () => {
-    const NotSubmitted = () => {
-      return (
-        <div className="text-center bg-red-300 text-red-800 w-1/2 rounded-full">
-          Not submit
-        </div>
-      );
-    };
-    const Submitted = () => {
-      return (
-        <div className="text-center bg-green-300 text-green-800 w-1/2 rounded-full">
-          Submitted
-        </div>
-      );
-    };
-  
-    const tableData = [
-      {
-        name: "Tadsika Khongkasawan",
-        email: "Tadsika@odds.team",
-        phone: "091999999",
-        contact: "Jill(0888888888)",
-        site: "PEA",
-        status: true,
-      },
-      {
-        name: "Tadsika Khongkasawan",
-        email: "Tadsika@odds.team",
-        phone: "0818888888",
-        contact: "Jill(0888888888)",
-        site: "PEA",
-        status: true,
-      },
-      {
-        name: "Tadsika Khongkasawan",
-        email: "Tadsika@odds.team",
-        phone: "0818888888",
-        contact: "Jill(0888888888)",
-        site: "PEA",
-        status: true,
-      },
-      {
-        name: "Tadsika Khongkasawan",
-        email: "Tadsika@odds.team",
-        phone: "0818888888",
-        contact: "Jill(0888888888)",
-        site: "PEA",
-        status: true,
-      },
-      {
-        name: "Tadsika Khongkasawan",
-        email: "Tadsika@odds.team",
-        phone: "0818888888",
-        contact: "Jill(0888888888)",
-        site: "PEA",
-        status: true,
-      },
-      {
-        name: "Tadsika Khongkasawan",
-        email: "Tadsika@odds.team",
-        phone: "0818888888",
-        contact: "Jill(0888888888)",
-        site: "PEA",
-        status: true,
-      },
-     
-    ];
+const SummaryTable = () => {
+  const NotExport = () => {
     return (
-      <table className="text-left">
-        <thead className="border-b-2 mb-2">
-          <tr>
-            <th className="w-2/12">Name</th>
-            <th className="w-2/12">Email</th>
-            <th className="w-2/12">Phone Number</th>
-            <th className="w-2/12">Contact</th>
-            <th className="w-2/12">Site</th>
-            <th className="w-2/12">Status</th>
-            <th className="w-2/12">Bell</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.map((data) => {
-            return (
-              <tr className="h-10">
-                <td>{data.name}</td>
-                <td>{data.email}</td>
-                <td>{data.phone}</td>
-                <td>{data.contact}</td>
-                <td>{data.site}</td>
-                {data.status ? (
-                  <td>
-                    <Submitted />
-                  </td>
-                ) : (
-                  <td>
-                    <NotSubmitted />
-                  </td>
-                )}
-  
-                <td>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24px"
-                    viewBox="0 0 24 24"
-                    width="24px"
-                    fill="#444444"
-                  >
-                    <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z" />
-                  </svg>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="text-center bg-gray-200 text-gray-500 w-2/2 rounded-full">
+        Not Export
+      </div>
     );
   };
-  export default Table;
-  
+  const Exported = () => {
+    return (
+      <div className="text-center bg-blue-300 text-black-800 w-2/2 rounded-full">
+        Exported
+      </div>
+    );
+  };
+
+  const tableData = [
+    {
+      name: "Tadsika Khongkasawan",
+      email: "Tadsika@odds.team",
+      phone: "0818888888",
+      accountName: "Jill(0888888888)",
+      accountNumber: "1234567890",
+      submitDate: "12/12/2021",
+      totalSalary: "300",
+      status: true,
+      transferBy: "PP",
+    },
+    {
+      name: "Tadsika Khongkasawan",
+      email: "Tadsika@odds.team",
+      phone: "0818888888",
+      accountName: "Jill(0888888888)",
+      accountNumber: "1234567890",
+      submitDate: "12/12/2021",
+      totalSalary: "300",
+      status: true,
+      transferBy: "PP",
+    },
+    {
+      name: "Tadsika Khongkasawan",
+      email: "Tadsika@odds.team",
+      phone: "0818888888",
+      accountName: "Jill(0888888888)",
+      accountNumber: "1234567890",
+      submitDate: "12/12/2021",
+      totalSalary: "300",
+      status: true,
+      transferBy: "PP",
+    },
+    {
+      name: "Tadsika Khongkasawan",
+      email: "Tadsika@odds.team",
+      phone: "0818888888",
+      accountName: "Jill(0888888888)",
+      accountNumber: "1234567890",
+      submitDate: "12/12/2021",
+      totalSalary: "300",
+      status: true,
+      transferBy: "PP",
+    },
+    {
+      name: "Tadsika Khongkasawan",
+      email: "Tadsika@odds.team",
+      phone: "0818888888",
+      accountName: "Jill(0888888888)",
+      accountNumber: "1234567890",
+      submitDate: "12/12/2021",
+      totalSalary: "300",
+      status: true,
+      transferBy: "PP",
+    },
+    {
+      name: "Tadsika Khongkasawan",
+      email: "Tadsika@odds.team",
+      phone: "0818888888",
+      accountName: "Jill(0888888888)",
+      accountNumber: "1234567890",
+      submitDate: "12/12/2021",
+      totalSalary: "300",
+      status: true,
+      transferBy: "PP",
+    },
+  ];
+  return (
+    <table className="text-center w-full">
+      <thead className="border-b-2 mb-2">
+        <tr>
+          <th className="w-2/12 text-left">Full Name</th>
+          <th className="w-2/12">Email</th>
+          <th className="w-1/12">Phone</th>
+          <th className="w-1/12">Account Name</th>
+          <th className="w-2/12 pl-5">Account No.</th>
+          <th className="w-1/12">Submit date</th>
+          <th className="w-1/12">Total salary</th>
+          <th className="w-1/12">Status</th>
+          <th className="w-2/12">Transfer By</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableData.map((data) => {
+          return (
+            <tr className="h-10">
+              <td className="text-left">{data.name}</td>
+              <td>{data.email}</td>
+              <td>{data.phone}</td>
+              <td>{data.accountName}</td>
+              <td className="pl-5">{data.accountNumber}</td>
+              <td>{data.submitDate}</td>
+              <td>{data.totalSalary}</td>
+              {data.status ? (
+                <td>
+                  <Exported />
+                </td>
+              ) : (
+                <td>
+                  <NotExport />
+                </td>
+              )}{" "}
+              <td>{data.transferBy}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+export default SummaryTable;
