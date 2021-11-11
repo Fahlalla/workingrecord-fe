4e69a214b1c8371bc03887e743fd7a95f63e3c21
fahlalla <tadsika.kkw@gmail.com>
2021-11-11 22:12:26 +0700

    Yaki changes source of data in Card.js

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -3,7 +3,7 @@
 const Card = (props) => {
   return (
     <div className="p-6 text-xl rounded-md text-blue-400  px-14 hover:text-gray-500 bg-gray-100">
-      <button onClick={() => props.select(props.data.name)}>
+      <button onClick={() => props.select(props.data.id)}>
         <div className="font-bold ">{props.data.name}</div>
         <div className="flex justify-between text-black ">
           <p>โอนแล้ว</p>
