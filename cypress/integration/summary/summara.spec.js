2f4001c2c8b720d0edd55a42cfd391fdf2474182
ศุภณัฐ ชินราช <45527907+dudkinox@users.noreply.github.com>
2021-11-17 08:20:43 +0700

    scenario เข้าหน้า summary

--- cypress/integration/summary/summara.spec.js
+++ cypress/integration/summary/summara.spec.js
@@ -1,27 +1,28 @@
-describe("open link", () => {
-  it("link page summary", () => {
+describe("เข้าหน้า summary", () => {
+  it("พี่รูฟ login มา คลิ๊ก menu Summary ", () => {
     cy.visit("http://localhost:3000/summary");
   });
-  it("check wording summary page", () => {
+  it("หน้า Summary ถูกแสดงขึ้นมา โดยที่ ลำดับบนสุดของหน้า มีคำว่า 'summary'", () => {
     cy.get(".text-3xl", { timeout: 4000 }).contains("Summary");
   });
 });
 
-describe("click tab All list", () => {
-  it("click tab", () => {
-    cy.get("[href='/summary?tab=allList'] > .py-2")
-      .contains("รายการทั้งหมด")
+describe("พบแท็บ รายการทั้งหมด, คนที่กรอกแล้ว, คนที่ยังไม่ได้กรอก ", () => {
+  it("tab one", () => {
+    cy.get("[href='/summary?tab=allList'] > .py-2").contains("รายการทั้งหมด");
   });
 
-  it("check tab Name list", () => {
-    cy.get(".py-2")
-    .contains("คนที่กรอกแล้ว")
-  })
+  it("tab two", () => {
+    cy.get(".py-2").contains("คนที่กรอกแล้ว");
+  });
 
-  it("check tab Name list", () => {
-    cy.get("[href='/summary?tab=nonexportList'] > .py-2")
-    .contains("คนที่ยังไม่ได้กรอก")
-  })
+  it("tab three", () => {
+    cy.get("[href='/summary?tab=nonexportList'] > .py-2").contains(
+      "คนที่ยังไม่ได้กรอก"
+    );
+  });
 
+  it("defalut active tab one", () => {
+    // cy.get("").
+  });
 });
-
